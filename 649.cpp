a407dbe1c877eed5f203e338e8d002876d6ae062
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2022-04-03 23:49:44 +0500

    Add files via upload

---
+++ 649.cpp
@@ -0,0 +1,37 @@
+﻿#include <fstream>
+#include <iostream>
+#include <map>
+using namespace std;
+
+int main()
+{
+	ifstream in("in.txt");
+	ofstream out("out.txt");
+	int N, K;
+	in >> N >> K;
+	string S;
+	in >> S;
+	map<char, int> m;
+	map<char, int>::iterator it;
+	for (int i = 0; i < N; i++)
+		m[S[i]] = 0;
+	int c = S.length();
+
+	for (int i = 0; i < S.length()-1; i++) {
+		m[S[i]]++;
+		for (int j = i+1; j < S.length(); j++) {
+			m[S[j]]++;
+			int n = 0;
+			for (it = m.begin(); it != m.end(); it++)
+				if (it->second > K)
+					n++;
+			if (n != 0)
+				break;
+			else { c++; }
+			}
+		for (it = m.begin(); it != m.end(); it++)
+			it->second = 0;
+	}
+	
+	out << c;
+}
---
+++ 867.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+ 
+
+int main()
+{
+
+	ifstream in("in.txt");
+	ofstream out("out.txt");
+	int N, D;
+	in >> N >> D;
+	vector<int> v;
+	map<int, int> m;
+	vector<int> res;
+	for (int i = 0; i < N; i++) {
+		int a;
+		in >> a;
+		v.push_back(a);
+		m[a] = 0;
+		res.push_back(a);
+	}
+	sort(v.begin(), v.end(), greater<int>());
+	int max = -1234;
+	int k = 0;
+	for (int i = 0; i < v.size() - 1; i++) {
+		for (int j = i; j < v.size(); j++) {
+			if (v[i] - v[j] <= D) {
+				k++;
+			}
+		}
+		if (k > max) 
+			max = k ;
+		k = 0;
+	}
+	out << max << endl;
+
+	int c = 1;
+	for (int i = 0; i < v.size(); i++) {
+		if (c > max)
+			c = 1;
+		m[v[i]] = c;
+		c++;
+	}
+
+	for (int i = 0; i < v.size(); i++)
+		out << m[res[i]] << " ";
+
+}
